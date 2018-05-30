82e1b67568886f538c1ac393bafd3093c8ca9186
Sina Sadeghi <sina.sa@gmail.com>
2018-05-30 21:17:26 +1000

    update attributeextractor

--- attributeextractor.py
+++ attributeextractor.py
@@ -9,7 +9,7 @@ ticker = 'SPY'
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
 sorted_data = cursor.execute('''
-    SELECT close, high, low
+    SELECT open, high, low, close
     FROM stock_data
     WHERE ticker = ?
     ORDER BY date ASC
@@ -21,34 +21,54 @@ for i in range(252, len(sorted_data)-LOOK_AHEAD):
 
     result = []
 
-    if (sorted_data[i+LOOK_AHEAD][0] - sorted_data[i][0])/sorted_data[i][0] >= DESIRED_RETURN:
+    if (sorted_data[i+LOOK_AHEAD][3] - sorted_data[i][3])/sorted_data[i][3] >= DESIRED_RETURN:
         result.append(1)
     else:
-        result.append(-1)
+        result.append(0)
 
-    if sorted_data[i][0] > sorted_data[i-252][0]:
-        result.append(1)
-    else:
-        result.append(-1)
+    #Yearly momentum
+    result.append(
+        (sorted_data[i][3] - sorted_data[i-252][3])/sorted_data[i-252][3]
+    )
 
-    if sorted_data[i][0] > sorted_data[i-120][0]:
-        result.append(1)
-    else:
-        result.append(-1)
+    #Half-yearly momentum
+    result.append(
+        (sorted_data[i][3] - sorted_data[i-120][3])/sorted_data[i-120][3]
+    )
 
-    if sorted_data[i][0] > sorted_data[i-63][0]:
-        result.append(1)
-    else:
-        result.append(-1)
+    #Quarterly momentum
+    result.append(
+        (sorted_data[i][3] - sorted_data[i-63][3])/sorted_data[i-63][3]
+    )
 
-    if sorted_data[i][0] > sorted_data[i-21][0]:
-        result.append(1)
-    else:
-        result.append(-1)
+    #Monthly momentum
+    result.append(
+        (sorted_data[i][3] - sorted_data[i-21][3])/sorted_data[i-21][3]
+    )
 
-    if sorted_data[i][0] > sorted_data[i-5][0]:
-        result.append(1)
-    else:
-        result.append(-1)
+    #Weekly momentum
+    result.append(
+        (sorted_data[i][3] - sorted_data[i-5][3])/sorted_data[i-5][3]
+    )
 
-    print(result)
+    #Daily momentum
+    result.append(
+        (sorted_data[i][3] - sorted_data[i-1][3])/sorted_data[i-1][3]
+    )
+
+    #Gap
+    result.append(
+        (sorted_data[i][0] - sorted_data[i-1][3])/sorted_data[i-1][3]
+    )
+
+    #Days range as %
+    result.append(
+        ((sorted_data[i][1] - sorted_data[i][2])/sorted_data[i][3])
+    )
+
+    for j in range(len(result)):
+        if j == 0:
+            print(result[j], end=' ')
+        else:
+            print("%d:%s" % (j, result[j]), end=' ')
+    print("")
